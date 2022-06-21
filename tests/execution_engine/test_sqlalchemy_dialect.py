4e3b7f3b6d1eff04aec28e3f7fb93dd1ad25657a
adamsealAMEND <78503547+adamsealAMEND@users.noreply.github.com>
2022-06-21 11:36:20 -0400

    Delete tests/execution_engine directory

--- tests/execution_engine/test_sqlalchemy_dialect.py
+++
@@ -1,9 +0,0 @@
-from great_expectations.execution_engine.sqlalchemy_dialect import GESqlDialect
-
-
-def test_dialect_instantiation_with_string():
-    assert GESqlDialect("hive") == GESqlDialect.HIVE
-
-
-def test_dialect_instantiation_with_byte_string():
-    assert GESqlDialect(b"hive") == GESqlDialect.HIVE
